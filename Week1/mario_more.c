ddf264f6f759fbef374676d0a52d029610a954fa
pedrohdavelar <pedrohdavelar@gmail.com>
2020-01-27 00:00:43 -0300

    Finished mario-more exercise.

---
+++ Week1/mario_more.c
@@ -0,0 +1,51 @@
+#include <cs50.h>
+#include <stdio.h>
+
+//declaring functions to be used
+void printline(int l, int h);
+void printblock(int n);
+void printspace(int n);
+
+int main(void)
+{
+    int h; //height of the pyramid
+    do
+    {
+        h = get_int("Height: ");
+    }
+    while (h < 1 ||  h > 8); //pool for height until a positive integer smaller than is input
+    
+    for (int i = 1; i <= h; i++) //print h lines. starting i as 1 so it works well with printspace() and printblock()
+    {
+        printline(i, h);
+    }
+}
+
+//print the line n of the piramid with height h; modified code from the first mario exercise
+//to add two empty spaces and then print the inverted pyramid
+void printline(int n, int h)
+{
+    printspace(h - n);
+    printblock(n);
+    printf("  ");
+    printblock(n);
+    printf("\n");
+}
+
+//print n `#` blocks 
+void printblock(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+}
+
+//print n ` ` spaces
+void printspace(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf(" ");
+    }  
+}
